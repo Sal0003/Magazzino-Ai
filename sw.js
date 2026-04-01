eaf9b6317a1b122920695fb479d376bce0bea9f4
Sal0003 <salvatore91.p@icloud.com>
2026-04-01 13:06:13 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,49 +1,5 @@
-const CACHE_NAME = 'magazzai-v18';
-const urlsToCache = [
-  './',
-  './index.html',
-  './style.css',
-  './app.js',
-  './manifest.json'
-];
-
-self.addEventListener('install', function(event) {
-  self.skipWaiting();
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(function(cache) {
-      return cache.addAll(urlsToCache);
-    })
-  );
-});
-
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    fetch(event.request).then(function(response) {
-      if (response && response.status === 200) {
-        var responseToCache = response.clone();
-        caches.open(CACHE_NAME).then(function(cache) {
-          cache.put(event.request, responseToCache);
-        });
-      }
-      return response;
-    }).catch(function() {
-      return caches.match(event.request);
-    })
-  );
-});
-
-self.addEventListener('activate', function(event) {
-  event.waitUntil(
-    caches.keys().then(function(cacheNames) {
-      return Promise.all(
-        cacheNames.filter(function(cacheName) {
-          return cacheName !== CACHE_NAME;
-        }).map(function(cacheName) {
-          return caches.delete(cacheName);
-        })
-      );
-    }).then(function() {
-      return self.clients.claim();
-    })
-  );
-});
+const CACHE_NAME = 'magazzai-v19';
+const urlsToCache = ['./', './index.html', './style.css', './app.js', './manifest.json'];
+self.addEventListener('install', function(event) { self.skipWaiting(); event.waitUntil(caches.open(CACHE_NAME).then(function(cache) { return cache.addAll(urlsToCache); })); });
+self.addEventListener('fetch', function(event) { event.respondWith(fetch(event.request).then(function(response) { if (response && response.status === 200) { var r = response.clone(); caches.open(CACHE_NAME).then(function(cache) { cache.put(event.request, r); }); } return response; }).catch(function() { return caches.match(event.request); })); });
+self.addEventListener('activate', function(event) { event.waitUntil(caches.keys().then(function(names) { return Promise.all(names.filter(function(n) { return n !== CACHE_NAME; }).map(function(n) { return caches.delete(n); })); }).then(function() { return self.clients.claim(); })); });
